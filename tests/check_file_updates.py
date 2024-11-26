3e18d14c1d87de850ddd70f2424ae50afa772862
Peter Lovett <pelovett@gmail.com>
2024-11-26 08:21:53 -0800

    ajdklfa;

--- tests/check_file_updates.py
+++ tests/check_file_updates.py
@@ -7,6 +7,7 @@ def check_for_changed_files(filepaths: List[str]):
     warnings = []
     print(f"Running with paths: {filepaths}")
     if "a/b.txt" in filepaths and "a/c.txt" not in filepaths:
+        print(f"User has edited a/b.txt but not a/c.txt")
         warnings.append(
             "If you're changing\n```\n/arion/arion-kraken/scripts/etl/kraken_traffic/rebuild/gen_rebuild_reph_kraken_traffic.sql\n```\nmake sure to also update\n```\n/arion/arion-kraken/scripts/etl/kraken_traffic/gen_reph_kraken_traffic.sqls\n```"
         )
