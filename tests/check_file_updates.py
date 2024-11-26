e36d8dabfd0cb7f35a620f377e25e2752352f59b
Peter Lovett <pelovett@gmail.com>
2024-11-26 08:00:19 -0800

    changing stuff

--- tests/check_file_updates.py
+++ tests/check_file_updates.py
@@ -7,17 +7,12 @@ def check_for_changed_files(filepaths: List[str]):
     warnings = []
     print(f"Running with paths: {filepaths}")
     if "a/b.txt" in filepaths and "a/c.txt" not in filepaths:
-        warnings.append("""If you're changing
-            ```
-            /arion/arion-kraken/scripts/etl/kraken_traffic/rebuild/gen_rebuild_reph_kraken_traffic.sql
-            ```
-            make sure to also update
-            ```
-            /arion/arion-kraken/scripts/etl/kraken_traffic/gen_reph_kraken_traffic.sqls
-            ```""")
+        warnings.append(
+            "If you're changing\n```\n/arion/arion-kraken/scripts/etl/kraken_traffic/rebuild/gen_rebuild_reph_kraken_traffic.sql\n```\nmake sure to also update\n```\n/arion/arion-kraken/scripts/etl/kraken_traffic/gen_reph_kraken_traffic.sqls\n```"
+        )
 
-    with open(os.environ["GITHUB_OUTPUT"], 'a') as output_buf:
-        warnings_str = '\n'.join(warnings)
+    with open(os.environ["GITHUB_OUTPUT"], "a") as output_buf:
+        warnings_str = "\n".join(warnings)
         output_buf.write(f"warnings={warnings_str}")
 
 
