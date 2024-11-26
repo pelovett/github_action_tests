f3c731583c43fe51556c5ccef956e1d607256013
Peter Lovett <pelovett@gmail.com>
2024-11-26 10:04:27 -0800

    aslkdfj

--- tests/check_file_updates.py
+++ tests/check_file_updates.py
@@ -6,10 +6,10 @@ from typing import List
 def check_for_changed_files(filepaths: List[str]):
     warnings = []
     print(f"Running with paths: {filepaths}")
-    if "a/b.txt" in filepaths and "a/c.txt" not in filepaths:
+    if "arion/arion-kraken/scripts/etl/kraken_traffic/rebuild/gen_rebuild_reph_kraken_traffic.sql" in filepaths and "arion/arion-kraken/scripts/etl/kraken_traffic/gen_reph_kraken_traffic.sql" not in filepaths:
         print(f"User has edited a/b.txt but not a/c.txt")
         warnings.append(
-            "If youre changing\n\`\`\`\n/arion/arion-kraken/scripts/etl/kraken_traffic/rebuild/gen_rebuild_reph_kraken_traffic.sql\n\`\`\`\nmake sure to also update\n\`\`\`\n/arion/arion-kraken/scripts/etl/kraken_traffic/gen_reph_kraken_traffic.sqls\n\`\`\`"
+            "If youre changing\n\`\`\`\n/arion/arion-kraken/scripts/etl/kraken_traffic/rebuild/gen_rebuild_reph_kraken_traffic.sql\n\`\`\`\nmake sure to also update\n\`\`\`\n/arion/arion-kraken/scripts/etl/kraken_traffic/gen_reph_kraken_traffic.sql\n\`\`\`"
         )
 
     with open(os.environ["GITHUB_OUTPUT"], "a") as output_buf:
